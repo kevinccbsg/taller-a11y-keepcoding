6825ae2521ced6198d43acd5126fedc07db04648
kevinccbsg <kevinccbsg@gmail.com>
2024-04-16 23:04:06 +0200

    feat: add solution

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,9 +1,23 @@
 import { createBrowserRouter } from 'react-router-dom';
 import InaccessibleHome from './pages/inaccessible/Home/Home';
 import InaccessibleBlog from './pages/inaccessible/Blog/Blog';
+import AccessibleHome from './pages/accessible/Home/Home';
+import AccessibleBlog from './pages/accessible/Blog/Blog';
 import CheckboxPage from './pages/checkbox/Checkbox';
 
 const router = createBrowserRouter([
+  {
+    path: '/',
+    element: (
+      <AccessibleHome />
+    ),
+  },
+  {
+    path: '/blog',
+    element: (
+      <AccessibleBlog />
+    ),
+  },
   {
     path: '/inaccessible',
     element: (
